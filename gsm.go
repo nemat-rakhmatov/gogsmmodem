e02ada41ca375c650670913e142c5860deab7266
nemat-rakhmatov <41049927+nemat-rakhmatov@users.noreply.github.com>
2022-11-08 21:25:52 +0500

    Update gsm.go

--- gsm.go
+++ gsm.go
@@ -331,7 +331,11 @@ func (self *Modem) init() error {
 	// get SMSC
 	// the modem complains if SMSC hasn't been set, but stores it correctly, so
 	// query for stored value, then send a set from the query response.
-	r, err := self.send("+CSCA?")
+	r, err := self.send("+CSCA=","+998991396700")
+	if err != nil {
+		return err
+	}
+	r, err = self.send("+CSCA?")
 	if err != nil {
 		return err
 	}
